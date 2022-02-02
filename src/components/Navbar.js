12915606bc0c697b44a653edf072d5d494c796d4
shardhayv <shardhayvatshyayan7@gmail.com>
2022-02-02 16:35:24 +0545

    Average looking dark mode added with some more textutil functions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav
+      className={`navbar navbar-expand-lg navbar- ${props.mode} bg-${props.mode}`}
+    >
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
         </a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler "
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -33,6 +34,7 @@ export default function Navbar(props) {
               </a>
             </li>
           </ul>
+
           <form className="d-flex">
             <input
               className="form-control me-2"
@@ -44,11 +46,22 @@ export default function Navbar(props) {
               Search
             </button>
           </form>
+          <div className={`form-check form-switch  text-${props.mode==='light'?'dark':'light'}`}  >
+            <input
+              className="form-check-input mx-1" 
+              type="checkbox"
+              onClick={props.toggleMode}
+              role="switch"
+              id="flexSwitchCheckDefault"
+            />
+            <label className={"form-check-label"} htmlFor="flexSwitchCheckDefault">
+              Enable DarkMode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
   );
 }
 
-Navbar.propTypes = {title : PropTypes.string.isRequired,
-}
+Navbar.propTypes = { title: PropTypes.string.isRequired };
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,61 +1,86 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-    // uppercase converter
-    const handleUpClick = ()=>{
+  // uppercase converter
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  // lower case converter
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  // clear text from util
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+  // copies text from the form area
+  const handleCopyClick = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
 
-        let newText = text.toUpperCase();
-        setText(newText);
+  // clear text from util
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
 
-    }
-    // lower case converter
-    const handleLowClick = ()=>{
-
-        let newText = text.toLowerCase();
-        setText(newText);
-
-    }
-    // clear text from util
-    const handleClearClick = ()=>{
-
-        let newText = '';
-        setText(newText);
-
-    }
-
-    const handleOnChange = (event)=>{
-
-        setText(event.target.value);
-
-    }
-
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
   const [text, setText] = useState("");
   return (
-      <>
-    <div className="container">
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="exampleFormControlTextarea1"
-          rows="8"
-        ></textarea>
-      </div >
-      <button className="btn btn-primary m-2" onClick={handleUpClick}>Convert to uppercase</button>
-      <button className="btn btn-primary m-2" onClick={handleLowClick}>Convert to Lowercase</button>
-      <button className="btn btn-primary m-2" onClick={handleClearClick}>Clear</button>
-    </div>
-    <div className="container my-3">
+    <>
+      <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{backgroundColor: props.mode==='dark'?'grey':'white' , color:props.mode==='dark'?'white':'#042743' }  }
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary m-2" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
+        <button
+          className="btn btn-primary mx-2 my-2 "
+          onClick={handleClearClick}
+        >
+          {" "}
+          Clear
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleCopyClick}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleExtraSpaces}>
+          Clear Extra Spaces
+        </button>
+      </div>
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
         <h2>Estimated Reading Time</h2>
-        <p>Average person will take {0.008*text.split(" ").length} minutes to read the above text.</p>
+        <p>
+          Average person will take {0.008 * text.split(" ").length} minutes to
+          read the above text.
+        </p>
         <h2>Preview</h2>
         <p>{text}</p>
-    </div>
+      </div>
     </>
   );
 }
